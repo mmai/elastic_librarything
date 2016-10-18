a63bcfe8625a26e9d3b2849eb7cdaaa9b1af1521
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-10-18 13:50:48 +0200

    years selection

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 build:
 	docker-compose up -d
 start:
-	docker-compose start
+	# docker-compose start
+	docker start elasticlibrarything_elasticsearch-int_1
 status:
 	curl http://127.0.0.1:9200/_cat/indices
 reset:
 	node ./bin/elastic-librarything.js aipotu --reset
 view:
-	firefox http://127.0.0.1:5601
+	# firefox http://127.0.0.1:5601
+	firefox index.html
--- js/main.js
+++ js/main.js
@@ -4,34 +4,76 @@ var client = new elasticsearch.Client({
     host: elasticsearchUrl
   });
 
+var model = initModel();
+initView(model);
 
-var year = 2013;
-var pBuy = getBooksByMonth(year, 'buy');
-var pStart = getBooksByMonth(year, 'start');
-var pEnd = getBooksByMonth(year, 'end');
-
-Promise.all([pBuy, pStart, pEnd]).then(function(res){
-    var buyed = getBookData(res[0]);
-    buyed.unshift('buyed');
-    var started = getBookData(res[1]);
-    started.unshift('started');
-    var ended = getBookData(res[2]);
-    ended.unshift('ended');
-
-    var data = {
-      columns: [buyed, started, ended],
-      type: 'bar'
+function initModel(){
+  var year = 2006;
+  var to = (new Date()).getFullYear()
+  var years = [];
+  while (year < to){
+    years.push(year);
+    year += 1;
+  }
+  
+  return {
+    input: {
+      years: years
     }
+  };
+}
 
-    var chart = c3.generate({
-        data: data,
-        axis: { x: { type: 'categorized' } },
-        bar: { width: { ratio: 0.3, }, }
-      });
-});
+function initView(model){
+  //years input
+  var yearsInput = document.querySelector('#years');
+  while (yearsInput.firstChild) {
+    yearsInput.removeChild(yearsInput.firstChild);
+  }
+  var elem;
+  for (var i=0, len = model.input.years.length; i < len ; i++){
+    elem = document.createElement("option"); 
+    elem.text = model.input.years[i]
+    yearsInput.appendChild(elem)
+  }
 
-function getBooksByMonth(year, type){
-  var request = {
+  yearsInput.addEventListener("change", function(){
+      generateYearStats(this.value);
+    }, false);
+}
+
+
+function generateYearStats(year){
+  var pBuy = getBooksByMonth(year, 'buy');
+  var pStart = getBooksByMonth(year, 'start');
+  var pEnd = getBooksByMonth(year, 'end');
+
+  Promise.all([pBuy, pStart, pEnd]).then(function(res){
+      var buyed = getBookData(res[0]);
+      buyed.unshift('buyed');
+      var started = getBookData(res[1]);
+      started.unshift('started');
+      var ended = getBookData(res[2]);
+      ended.unshift('ended');
+
+      var data = {
+        columns: [ buyed, started, ended],
+        type: 'bar'
+      }
+
+      var chart = c3.generate({
+          axis: {
+            x: {
+              type: 'category',
+              categories: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec']
+            }
+          },
+          data: data,
+          bar: { width: { }, }
+        });
+    });
+
+  function getBooksByMonth(year, type){
+    var request = {
       index: elasticsearchIndex,
       size: 5,
       body: {
@@ -58,13 +100,14 @@ function getBooksByMonth(year, type){
     };
     request.body.query.range[type + "_date"] = { from: year + "-01-01", to: year + "-12-31" }
     return client.search(request); 
-}
+  }
 
-function getBookData(elasticRes){
-  var res = elasticRes.aggregations.books_by_month.buckets;
-  var data = res.map(function(m){
-      return m.doc_count
-    });
-  return data;
-}
+  function getBookData(elasticRes){
+    var res = elasticRes.aggregations.books_by_month.buckets;
+    var data = res.map(function(m){
+        return m.doc_count
+      });
+    return data;
+  }
 
+}
